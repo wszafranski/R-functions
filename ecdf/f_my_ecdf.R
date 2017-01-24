6f542b66bfade0aa9983bb1067c84c895750efdb
wszafranski <wszafranski@gmail.com>
2017-01-23 17:23:14 -0700

    streamlined file to make faster
    Used the duplicated function to make my function must faster

--- ecdf/f_my_ecdf.R
+++ ecdf/f_my_ecdf.R
@@ -1,57 +1,55 @@
 
-# This function calculates the empirical CDF of a given dataset.
-# The function needs just one sequence/vector of data of any length.
-# It will give a matrix of values with n rows and 5 columns
-# showing how the calculations were performed.
-
-my_ecdf=function(x)
-{
-  #x = floor(runif(100, min=0, max=75))
+my_ecdf = function (x){
+  
   n = length(x)
   
-  ecdf_data = matrix(NA, nrow = n, ncol=5)
-  colnames(ecdf_data) = c("values sorted","rank","percentile","rank-check","rev-percentile")
+  ecdf_data = matrix(NA, nrow = n, ncol=6)
+  colnames(ecdf_data) = c("values_sorted","rank","bad_percentile","duplicate","final_percentile","fdc_percentile")
   
   ecdf_data[,1] = sort(x)
   ecdf_data[,2] = 1:n
   ecdf_data[,3] = ecdf_data[,2]/(n+1)
   
-  #there will probably be ties (values with same value [,1] but increasing rank & percentile [,3] )
-  #adjust ties to the max probability
+  # there will probably be ties (values with same value in column 1, 
+  # but increasing rank & percentile [,3] )
+  # adjust ties to the max probability
   
-  for (i in 1:n){
+  
+  # flag duplicate values with TRUE (1). This first instance
+  # isn't flagged, only the 2nd, 3rd, etc.
+  # 'fromLast=TRUE' makes this occur from the bottom - the way I want.
+  ecdf_data[,4] = duplicated(ecdf_data[,1], fromLast = TRUE)
+  
+  
+  # Fill in the "revised" percentile in the case of ties
+  for (i in n:1){
+    #13, 12, 11, 10, 9
     
-    # if ranks are tied, then give placeholder 0
-    # if not, give placeholder of 1
-    # give the last value a 1 (no tie or it is the max value if it is tied)
-    if (i == n){
-      ecdf_data[i,4] = 1
-      
-    }else if (ecdf_data[i,1] == ecdf_data[(i+1),1]){
-      ecdf_data[i,4] = 0
-      
-    }else {ecdf_data[i,4] = 1}
+    if(ecdf_data[i, 4] == 0){
+      ecdf_data[i, 5] = ecdf_data[i, 3]
+    }else {
+      ecdf_data[i, 5] = ecdf_data[(i+1), 5]
+    }
+    
   }
   
-  # Subset the max values (ecdf_data[,4]==1)
-  maxtievalues = subset(ecdf_data, ecdf_data[,4]==1)
-  nmaxtie = length(maxtievalues[,1])
-  temp4 = matrix(NA, nrow=1, ncol=5)
+  # Make the last column flow duration percentiles (1- col 5)
+  # as opposed to CDF percentiles
+  ecdf_data[, 6] = 1-ecdf_data[, 5]
   
+
+  NOTE_ON_FUNCTION_OUTPUT = c("The original values sorted", "Rank of the sorted values", 
+                              "Original (bad) CDF Percentile", "Check for Duplicate Values", 
+                              "Final CDF Percentile", "Flow Duration Curve Percentiles",
+                              " ")
+
+  Output <- data.frame(NOTE_ON_FUNCTION_OUTPUT, row.names = c("Columm 1", "Columm 2", 
+                                                              "Columm 3", "Columm 4",
+                                                              "Column 5", "Column 6",
+                                                              " "))
+  print(Output, justify = c("left"))
   
-  # Adjust the tied percentiles to the max percentile
-  for (i in 1:nmaxtie){
-    temp3 = subset(ecdf_data, ecdf_data[,1] == maxtievalues[i,1])
-    temp3[,5] = maxtievalues[i,3]
-    temp4 = rbind(temp4, temp3)
-  }
-  
-  # save in a new data frame
-  ecdf_data2 = temp4[2:length(temp4[,1]),]
-  
-  rm(ecdf_data, maxtievalues, nmaxtie, temp3, temp4)
-  
-  #ecdf_data2
-  return(ecdf_data2)
+  ecdf_data
   
 }
+  
